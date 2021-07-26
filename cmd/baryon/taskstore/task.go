793a818a985ab03b98f61e1bd4990a10e49f4abd
Alexander Pikeev <alexanderpikeev@gmail.com>
2021-07-26 21:02:51 +0300

    first

---
+++ cmd/baryon/taskstore/task.go
@@ -0,0 +1,135 @@
+package taskstore
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type Task struct {
+	Id int `json:"id"`
+	Text string `json:"text"`
+	Tags []string `json:"tags"`
+	Due time.Time `json:"due"`
+}
+
+type TaskStore struct {
+	sync.Mutex
+
+	tasks map[int]Task
+	nextId int
+}
+
+func New() *TaskStore {
+	ts := &TaskStore{}
+	ts.tasks = make(map[int]Task)
+	ts.nextId = 0
+
+	return ts
+}
+
+// CreateTask создает новую задачу и сохраняет её в хранилище TaskStore.tasks
+func (ts *TaskStore) CreateTask(text string, tags []string, due time.Time) int {
+	ts.Lock()
+	defer ts.Unlock()
+
+	task := Task{
+		Id: ts.nextId,
+		Text: text,
+		Due: due,
+	}
+	task.Tags = make([]string, len(tags))
+	copy(task.Tags, tags)
+
+	ts.tasks[ts.nextId] = task
+	ts.nextId++
+
+	return task.Id
+}
+
+// GetTask возвращает задачу с заданным ID
+// Если задачи с таким ID не существует - будет возвращена ошибка
+func (ts *TaskStore) GetTask(id int) (Task, error) {
+	ts.Lock()
+	defer ts.Unlock()
+
+	t, ok := ts.tasks[id]
+	if ok {
+		return t, nil
+	} else {
+		return Task{}, fmt.Errorf("task with id=%d not found", id)
+	}
+}
+
+// DeleteTask удаляет задачу с заданным ID
+// Если задачи с таким ID не существует - будет возвращена ошибка
+func (ts *TaskStore) DeleteTask(id int) error {
+	ts.Lock()
+	defer ts.Unlock()
+
+	if _, ok := ts.tasks[id]; !ok {
+		return fmt.Errorf("task with id=%d not found", id)
+	}
+
+	delete(ts.tasks, id)
+	return nil
+}
+
+// DeleteAllTasks удаляет из хранилища все задачи
+func (ts *TaskStore) DeleteAllTasks() error {
+	ts.Lock()
+	defer ts.Unlock()
+
+	ts.tasks = make(map[int]Task)
+	return nil
+}
+
+func (ts *TaskStore) GetAllTasks() []Task {
+	ts.Lock()
+	defer ts.Unlock()
+
+	allTasks := make([]Task, 0, len(ts.tasks))
+	for _, task := range ts.tasks {
+		allTasks = append(allTasks, task)
+	}
+
+	return allTasks
+}
+
+
+// GetTaskByTag возвращает задачи с заданным тегом
+func (ts *TaskStore) GetTaskByTag(tag string) []Task {
+	ts.Lock()
+	defer ts.Unlock()
+
+	var tasks []Task
+
+taskloop:
+	for _, task := range ts.tasks {
+		for _, taskTag := range task.Tags {
+			if taskTag == tag {
+				tasks = append(tasks, task)
+				continue taskloop
+			}
+		}
+	}
+
+	return tasks
+}
+
+// GetTaskByDueDate возвращает задачи, которые запланированы на заданную дату
+func (ts *TaskStore) GetTaskByDueDate(year int, month time.Month, day int) []Task {
+	ts.Lock()
+	defer ts.Unlock()
+
+	var tasks []Task
+
+	for _, task := range ts.tasks {
+		y, m, d := task.Due.Date()
+		if y == year && m == month && d == day {
+			tasks = append(tasks, task)
+		}
+	}
+
+	return tasks
+}
